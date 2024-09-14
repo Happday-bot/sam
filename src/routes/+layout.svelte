d17fab743fa76e245b42ab591c4f3d39e0bf6582
sivaplaysmC <sivaram.asdf@gmail.com>
2024-09-14 19:13:22 +0530

    sample commit ^_^

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,15 @@
-<link rel="stylesheet" href="https://nordcdn.net/ds/css/3.2.1/nord.min.css" integrity="sha384-x2XdCI8Yog7KGRmrrGLegjFrrIYXEhGNxql/xEXdMoW5NkpEhlAkUHdQJxkL1vPg" crossorigin="anonymous" />
-<script type="module" src="https://nordcdn.net/ds/components/3.17.0/index.js" integrity="sha384-eme6DjBrFKzjO5DCscEfRDzjbMLA/MR4OcmSj48jogyZHps2ujPBo8+EdOZgEELg" crossorigin="anonymous"></script>
+<script
+	type="module"
+	src="https://nordcdn.net/ds/components/3.17.0/index.js"
+	integrity="sha384-eme6DjBrFKzjO5DCscEfRDzjbMLA/MR4OcmSj48jogyZHps2ujPBo8+EdOZgEELg"
+	crossorigin="anonymous"
+></script>
+
+<h1>Hi There Sam, this is a change i made!!!</h1>
+<link
+	rel="stylesheet"
+	href="https://nordcdn.net/ds/css/3.2.1/nord.min.css"
+	integrity="sha384-x2XdCI8Yog7KGRmrrGLegjFrrIYXEhGNxql/xEXdMoW5NkpEhlAkUHdQJxkL1vPg"
+	crossorigin="anonymous"
+/>
 <slot></slot>
